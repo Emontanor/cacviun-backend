60614d07bbfd2883aeaa0507ade4ff3a3891a7ee
jumontenegrol <jumontenegrol@unal.edu.co>
2025-11-18 10:01:13 -0500

    Creacion de initCategory en service

--- src/database/mongo.module.ts
+++ src/database/mongo.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { MongoClient } from "mongodb";
 
-const uri = "poner ruta MongoAtlas";
+//const uri = "mongodb://localhost:27017"; 
+const uri = "mongodb+srv://jumontenegrol:Juanmonlo2828.@cluster0.tu5cn67.mongodb.net/CacviUn?retryWrites=true&w=majority&tls=true&appName=Cluster0";
 const client = new MongoClient(uri);
 
 @Module({
