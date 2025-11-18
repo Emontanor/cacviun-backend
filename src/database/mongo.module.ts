eb730afaf3833179e1f6563878998d2bd5963095
Emontanor <fhernandezm@unal.edu.co>
2025-11-18 10:20:46 -0500

    Configuracion final del modulo database

--- src/database/mongo.module.ts
+++ src/database/mongo.module.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { MongoService } from "./mongo.service";
 import { MongoController } from "./mongo.controller";
 
-const uri = "mongodb+srv://jumontenegrol:Juanmonlo2828.@cluster0.tu5cn67.mongodb.net/CacviUn?retryWrites=true&w=majority&tls=true&appName=Cluster0";
+//const uri = "mongodb+srv://jumontenegrol:Juanmonlo2828.@cluster0.tu5cn67.mongodb.net/CacviUn?retryWrites=true&w=majority&tls=true&appName=Cluster0";
 //const uri = "mongodb+srv://fhernandezm_db_admin:Fhm1qazz@cluster0.tu5cn67.mongodb.net/CacviUn?retryWrites=true&w=majority&tls=true&appName=Cluster0";
 const client = new MongoClient(uri);
 
@@ -15,7 +15,8 @@ const client = new MongoClient(uri);
                 await client.connect();
                 return client.db();
             },
-        }
+        },
+        MongoService,
     ],
     controllers: [MongoController],
     exports: ["MONGO_DB",MongoService],
