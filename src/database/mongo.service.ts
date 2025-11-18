aec7d4c9d8f23a14865a0d05ee4a53e172a5c5bb
Emontanor <fhernandezm@unal.edu.co>
2025-11-18 10:10:04 -0500

    Modulo database

--- src/database/mongo.service.ts
+++ src/database/mongo.service.ts
@@ -1,37 +1,15 @@
-// src/database/mongo.service.ts
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { MongoClient } from 'mongodb';
+import { Inject, Injectable } from '@nestjs/common';
+import { Db } from 'mongodb';
 
 @Injectable()
-export class MongoService implements OnModuleInit {
-  private client: MongoClient;
-  private db: any;
+export class MongoService {
+  constructor(
+    @Inject("MONGO_DB") private readonly db: Db,
+  ) {}
 
-  async onModuleInit() {
-    this.client = new MongoClient(process.env.MONGOURI as string);
-    await this.client.connect();
-
-    this.db = this.client.db('CacviUn'); // TU BASE
-    console.log('MongoDB conectado a CacviUn');
-  }
-
-  getDb() {
+  getDb(): Db {
     return this.db;
   }
 
-  async testDb() {
-    const collections = await this.db.listCollections().toArray();
-    return { message: 'Conexión exitosa', collections };
-  }
-
-  async createRolesCollection() {
-    const roles = this.db.collection('Roles');
-
-    await roles.insertMany([
-      { role_id: 1, description: 'admin' },
-      { role_id: 2, description: 'user' },
-    ]);
-
-    return { message: 'Roles creados correctamente' };
-  }
+  
 }
