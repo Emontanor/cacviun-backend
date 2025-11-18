1976e5845cc4dac889c5af1ce2bd31adaa2df746
Emontanor <fhernandezm@unal.edu.co>
2025-11-18 08:41:14 -0500

    Commit inicial

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('test-db')
+  async testDb() {
+    return this.appService.testDbConnection();
+  }
+
+  @Get('init-roles')
+  async initRoles() {
+    return this.appService.initRoles();
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,48 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Db } from 'mongodb';
+
+@Injectable()
+export class AppService {
+
+  constructor(
+    @Inject("MONGO_DB") private readonly db: Db,
+  ){}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  testDbConnection(): Promise<string> {
+    return this.db.listCollections().toArray().then(() => 'Database connection successful');
+  }
+
+  async initRoles(): Promise<string> {
+    const collectionName = 'Roles';
+
+    // Verificar si la colección ya existe
+    const collections = await this.db.listCollections({ name: collectionName }).toArray();
+    
+    // Crear la colección si no existe
+    if (collections.length === 0) {
+      await this.db.createCollection(collectionName);
+      console.log(`Collection "${collectionName}" created.`);
+    }
+
+    const rolesCollection = this.db.collection(collectionName);
+
+    // Poblar los datos evitando duplicados
+    await rolesCollection.updateOne(
+      { role_id: 1 },
+      { $set: { role_id: 1, description: 'admin' } },
+      { upsert: true }
+    );
+
+    await rolesCollection.updateOne(
+      { role_id: 2 },
+      { $set: { role_id: 2, description: 'user' } },
+      { upsert: true }
+    );
+
+    return 'Roles collection initialized';
+  }
+}
