cee9046cabdfe74a2e7c15d741f65c9e67607214
Emontanor <fhernandezm@unal.edu.co>
2025-12-03 15:08:23 -0500

    Variables de ambiente de render

--- src/database/mongo.module.ts
+++ src/database/mongo.module.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { MongoService } from "./mongo.service";
 import { MongoController } from "./mongo.controller";
 
-const uri = "mongodb+srv://jumontenegrol_db_user:12345@cluster0.tu5cn67.mongodb.net/CacviUn?retryWrites=true&w=majority&tls=true&appName=Cluster0";
+const uri = process.env.MONGO_URI!;
 //const uri = "mongodb+srv://fhernandezm_db_admin:Fhm1qazz@cluster0.tu5cn67.mongodb.net/CacviUn?retryWrites=true&w=majority&tls=true&appName=Cluster0";
 const client = new MongoClient(uri);
 
